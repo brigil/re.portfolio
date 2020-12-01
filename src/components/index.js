d47e0768f363134bad7d060f1c3e70e2379f220d
Brigitte <urbrigette@gmail.com>
2020-12-01 10:56:17 -0500

    paused

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@ import "../styles/Navbar.css";
 
 export const Home = () => {
     return (
-        <><div className="body1"> <img class="introPic"
-            src="https://i.ibb.co/W5md5yb/1601429749499.jpg" /> <br /> <br /> <br />
-            <h3 class="hello">Hi, I'm Web Designer
+        <><div className="body1"> <img className="introPic"
+            src="https://i.ibb.co/W5md5yb/1601429749499.jpg" /> <br /> <br /> <br /><br />
+            <h3 class="hello"><img
+            src="https://fontmeme.com/permalink/201201/e52efc48454378556909114857a9771a.png" />
        <br />
-                <span> Brigitte Gil </span></h3>
-            <a href="aboutme.html">
+       <img
+            src="https://fontmeme.com/permalink/201201/d7f606b1509508702855049f85d30123.png"/></h3>
+            <a href="/aboutme"><br /><br /><br />
                 <button className="btn btn-outline-light">
                     Let's Design Together</button></a> </div></>
     )
---
+++ src/pages/aboutme.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+
+function aboutme() {
+	return (
+	<></>
+
+	)
+}
+export default aboutme;
