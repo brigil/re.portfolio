f829c8bc39c1814604e76ea5aa6a3d8cd4e00d40
Brigitte <urbrigette@gmail.com>
2020-11-30 20:55:14 -0500

    added picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import "../styles/Navbar.css";
 
 
 
 function Navbar() {
     return (
     
-       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+       <nav className="navbar navbar-expand-lg navbar-light">
       <a className="navbar-brand" href="./aboutme.html">
       <img src="https://fontmeme.com/temporary/55b66cd6cb9c8fdb303d88c74bd7d411.png" width="50" height="40"/>
       <img src="https://fontmeme.com/permalink/201201/87810fb898b2de25295d3cbf26606a9f.png" width="70" height="40"/></a>
@@ -22,17 +23,19 @@ function Navbar() {
 
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
 
-        <ul class="navbar-nav">
+      <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">Home</a>
+          </li>
+          
+          <li className="nav-item">
+            <a className="nav-link" href="/loginsignup">Portfolio </a>
+          </li>
 
-          <li class="nav-item">
-            <a id class="nav-link" href="./aboutme.html">About </a>
+          <li className="nav-item">
+            <a className="nav-link" href="/savedtours">Contact</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./portfolio.html">Portfolio </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./contact.html">Contact</a>
-          </li>
+
         </ul>
       </div>
     </nav>
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 export const Home = () => {
     return (
-       <> <h2>this is my home page</h2> </>
+       <> <img class="introPic" src="https://i.ibb.co/W5md5yb/1601429749499.jpg"/> <h3 class="hello">Hi, I'm Web Designer
+       <br/>
+      <span> Brigitte Gil </span></h3>
+      <a href="aboutme.html">
+      <button class="btn btn-outline-light">
+        Let's Design Together</button></a> </>
     )
 }
 export default Home;
