3e4eba3ca6c0c94b85a1f52d08ab5c879429893a
Brigitte <urbrigette@gmail.com>
2020-11-30 22:00:14 -0500

    added some style into the text

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@ import "../styles/Navbar.css";
 
 export const Home = () => {
     return (
-       <><div className="body1"> <img class="introPic" src="https://i.ibb.co/W5md5yb/1601429749499.jpg"/> <h3 class="hello">Hi, I'm Web Designer
-       <br/>
-      <span> Brigitte Gil </span></h3>
-      <a href="aboutme.html">
-      <button class="btn btn-outline-light">
-        Let's Design Together</button></a> </div></>
+        <><div className="body1"> <img class="introPic"
+            src="https://i.ibb.co/W5md5yb/1601429749499.jpg" /> <br /> <br /> <br />
+            <h3 class="hello">Hi, I'm Web Designer
+       <br />
+                <span> Brigitte Gil </span></h3>
+            <a href="aboutme.html">
+                <button className="btn btn-outline-light">
+                    Let's Design Together</button></a> </div></>
     )
 }
 export default Home;
